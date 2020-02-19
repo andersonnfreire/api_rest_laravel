de5fcac6ae98efbabeac8df50da7c6cfad53c30d
Anderson N Freire <coolerinformatica1995@gmail.com>
2020-02-19 17:24:31 -0300

    Inserindo produto via postman

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::namespace('Api')->name('api.')->group(function(){
+	Route::prefix('products')->group(function(){
+		Route::get('/','ProductController@index')->name('index_products');
+		Route::get('/{id}','ProductController@show')->name('single_products');
+		Route::post('/','ProductController@store')->name('store_products');
+	});
+});
